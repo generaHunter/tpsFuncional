256b46f9ae80358449c20fb0411e536ede0188f5
JoseM <JoseM@Jose>
2019-09-25 11:49:53 -0600

    CRUD Alumno

--- build/generated-sources/ap-source-output/Logica_Negocio/Usuario_.java
+++ build/generated-sources/ap-source-output/Logica_Negocio/Usuario_.java
@@ -9,7 +9,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-09-24T12:01:02")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-09-25T11:47:11")
 @StaticMetamodel(Usuario.class)
 public class Usuario_ { 
 
